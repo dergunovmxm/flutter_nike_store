a4e8d0cd905c360f970ce3f658dc26fbef15c05e
Derga <dergan73@mail.ru>
2023-05-17 16:28:59 +0400

    добавлена страницца товара

---
+++ lib/widgets/ItemBottomNavBar.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ItemBottomNavBar extends StatelessWidget {
+  const ItemBottomNavBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Color(0xFFF5F9FD),
+      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Container(
+            padding: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+            decoration: BoxDecoration(
+                color: Color(0xFF475269),
+                borderRadius: BorderRadius.circular(10),
+                boxShadow: [
+                  BoxShadow(
+                    color: Color(0xFF475269).withOpacity(0.3),
+                    blurRadius: 5,
+                    spreadRadius: 1,
+                  )
+                ]),
+            child: Row(
+              children: [
+                Text(
+                  'В корзину',
+                  style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.white),
+                ),
+                SizedBox(width: 10),
+                Icon(
+                  CupertinoIcons.cart_badge_plus,
+                  color: Colors.white,
+                  size: 30,
+                )
+              ],
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.symmetric(vertical: 5, horizontal: 15),
+            decoration: BoxDecoration(
+                color: Color(0xFF475269),
+                borderRadius: BorderRadius.circular(10),
+                boxShadow: [
+                  BoxShadow(
+                    color: Color(0xFF475269).withOpacity(0.3),
+                    blurRadius: 5,
+                    spreadRadius: 1,
+                  )
+                ]),
+            child: Row(
+              children: [
+                Icon(
+                  Icons.shopping_bag,
+                  color: Colors.white,
+                  size: 40,
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
